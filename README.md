fb41560994b6a169ebbeab5283190cc8066a7947
Florian Thomas <flo@florianthomas.net>
2018-06-21 14:10:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# find-reviewers
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that 
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how find-reviewers could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2018 Florian Thomas <flo@florianthomas.net> (https://github.com/Crunch09/find-reviewers)
---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+  app.on(`pull_request.labeled`, async context => {
+    const label = context.payload.label.name
+
+    context.log({event: context.event, label: label})
+    if(label == "ready-for-review"){
+      await assignReviewers(context)
+    }
+  })
+
+  async function assignReviewers(context) {
+    const owner = context.payload.repository.owner.login
+
+    let possibleReviewers = ["annebyrne", "Crunch09", "erikamorenosierra", "nikz", "qermyt", "RomainPiel"]
+    let index = possibleReviewers.indexOf(owner)
+    if (index > -1) {
+      possibleReviewers.splice(index, 1)
+    }
+    let firstReviewer = possibleReviewers[Math.floor(Maath.random()*possibleReviewers.length)]
+    index = possibleReviewers.indexOf(firstReviewer)
+    possibleReviewers.splice(index, 1)
+
+    let secondReviewer = possibleReviewers[Math.floor(Maath.random()*possibleReviewers.length)]
+
+    try {
+      const result = await context.github.pullRequests.createReviewRequest(context.issue({reviewers: [firstReviewer, secondReviewer]}))
+    } catch(error) {
+      context.log({ error: error })
+    }
+    context.log({result: result })
+  }
+}
